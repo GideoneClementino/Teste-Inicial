7a8a6ac63bd411c015fd42b58a687d2ff2b43ce2
GideoneSilva <gideonedeon@gmail.com>
2020-12-02 13:34:27 -0300

    Adicionando time de espera e estruturas finais do teste

--- src/test/java/testes/SignTest.java
+++ src/test/java/testes/SignTest.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.concurrent.TimeUnit;
+
 public class SignTest {
     @Test
     public void testeFazerLoginNoTaskit(){
@@ -14,15 +16,25 @@ public class SignTest {
         //Instanciando drive
         WebDriver navegador = new ChromeDriver();
 
+        //Adicionando tempo que o navegador irá esperar o elemento aparecer
+        navegador.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
         //Abrindo navegador
         navegador.get("http://www.juliodelima.com.br/taskit");
 
         //Buncando elemento e clicando
         navegador.findElement(By.linkText("Sign in")).click();
 
+        //Logando na pagina
         navegador.findElement(By.id("signinbox")).findElement(By.name("login")).sendKeys("gideone1");
         navegador.findElement(By.id("signinbox")).findElement(By.name("password")).sendKeys("12345");
-
         navegador.findElement(By.linkText("SIGN IN")).click();
+
+        //Testando se a mensagem inicial da pagina está correta
+        String saudacao = navegador.findElement(By.className("me")).getText();
+        Assert.assertEquals("Hi, Gideone", saudacao);
+
+        //Fechando navegador após teste terminar
+        navegador.quit();
     }
 }
